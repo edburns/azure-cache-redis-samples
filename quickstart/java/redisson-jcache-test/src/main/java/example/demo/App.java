cbfd4b0db4b2a768487e7fe7c52494ffc6e9d5ef
Ed Burns <edburns@microsoft.com>
2021-05-07 14:25:14 -0400

    On branch edburns-msft-dd-1323955-redisson-jcache Works for basic access. modified:   quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
+++ quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
@@ -15,45 +15,23 @@ public class App
 
   public void redisson() {
     MutableConfiguration<String, String> config = new MutableConfiguration<>();
-    
+
+    System.out.println("debug: edburns: obtaining reference to cache");
     CacheManager manager = Caching.getCachingProvider().getCacheManager();
-    Cache<String, String> cache = manager.createCache("namedCache", config);    
+    Cache<String, String> cache = manager.createCache("namedCache", config);
+
+    String now = "" + System.currentTimeMillis();
+    System.out.println("debug: edburns: storing value in cache: now: " + now);
+
+    cache.put("now", now);
+    System.out.println("debug: edburns: retrieving value from cache");
+    System.out.println("now: " + System.currentTimeMillis() + " then: " + cache.get("now"));
+    return;
   }
   
-    public static void main( String[] args )
-    {
-      new App().redisson();
-
-        boolean useSsl = true;
-        String cacheHostname = System.getenv("REDISCACHEHOSTNAME");
-        String cachekey = System.getenv("REDISCACHEKEY");
-
-        // Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
-        JedisShardInfo shardInfo = new JedisShardInfo(cacheHostname, 6380, useSsl);
-        shardInfo.setPassword(cachekey); /* Use your access key. */
-        Jedis jedis = new Jedis(shardInfo);      
-
-        // Perform cache operations using the cache connection object...
-
-        // Simple PING command        
-        System.out.println( "\nCache Command  : Ping" );
-        System.out.println( "Cache Response : " + jedis.ping());
-
-        // Simple get and put of integral data types into the cache
-        System.out.println( "\nCache Command  : GET Message" );
-        System.out.println( "Cache Response : " + jedis.get("Message"));
-
-        System.out.println( "\nCache Command  : SET Message" );
-        System.out.println( "Cache Response : " + jedis.set("Message", "Hello! The cache is working from Java!"));
-
-        // Demonstrate "SET Message" executed as expected...
-        System.out.println( "\nCache Command  : GET Message" );
-        System.out.println( "Cache Response : " + jedis.get("Message"));
-
-        // Get the client list, useful to see if connection list is growing...
-        System.out.println( "\nCache Command  : CLIENT LIST" );
-        System.out.println( "Cache Response : " + jedis.clientList());
-
-        jedis.close();
-    }
+  public static void main( String[] args ) {
+    App app = new App();
+    app.redisson();
+    System.exit(0);
+  }
 }
