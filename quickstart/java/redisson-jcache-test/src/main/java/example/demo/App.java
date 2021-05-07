b60d40d8e568540a9477552245dc2d38aa1c8798
Ed Burns <edburns@microsoft.com>
2021-05-07 19:38:39 -0400

    LocalTime WIP

--- quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
+++ quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
@@ -1,10 +1,9 @@
 package example.demo;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisShardInfo;
-
 import javax.cache.*;
 import javax.cache.configuration.*;
+
+import java.time.LocalTime;
 
 /**
  * Redis test
@@ -16,15 +15,14 @@ public class App
   public void redisson() {
     MutableConfiguration<String, String> config = new MutableConfiguration<>();
 
-    System.out.println("debug: edburns: obtaining reference to cache");
-    CacheManager manager = Caching.getCachingProvider().getCacheManager();
+    var manager = Caching.getCachingProvider().getCacheManager();
     Cache<String, String> cache = manager.createCache("namedCache", config);
 
-    String now = "" + System.currentTimeMillis();
-    System.out.println("debug: edburns: storing value in cache: now: " + now);
+    var t = LocalTime.now();
+
+    var now = "" + System.currentTimeMillis();
 
     cache.put("now", now);
-    System.out.println("debug: edburns: retrieving value from cache");
     System.out.println("now: " + System.currentTimeMillis() + " then: " + cache.get("now"));
     return;
   }
