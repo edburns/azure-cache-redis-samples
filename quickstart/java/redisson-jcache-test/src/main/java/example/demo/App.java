92cc174f90dd07f99ee5d480edad786df867cec2
Ed Burns <edburns@microsoft.com>
2021-05-07 20:22:31 -0400

    On branch edburns-msft-dd-1323955-redisson-jcache Thanks Stephen Colbourne. modified:   quickstart/java/redisson-jcache-test/pom.xml modified:   quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
+++ quickstart/java/redisson-jcache-test/src/main/java/example/demo/App.java
@@ -4,6 +4,8 @@ import javax.cache.*;
 import javax.cache.configuration.*;
 
 import java.time.LocalTime;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.lang.System.*;    
 
 /**
  * Redis test
@@ -12,23 +14,27 @@ import java.time.LocalTime;
 public class App 
 {
 
-  public void redisson() {
-    var now = LocalTime.now();
+  public void redisson(boolean removeFirst) {
     var manager = Caching.getCachingProvider().getCacheManager();
     Cache<String, LocalTime> cache =
       manager.createCache("namedCache", new MutableConfiguration()
                           .setStoreByValue(true)
                           .setTypes(String.class, LocalTime.class));
-    
-    cache.put("then", now);
-    System.out.println("That was then: " + cache.get("then") +
-                       ", this is now: " + LocalTime.now() + ".");
+
+    if (removeFirst) {
+      cache.remove("then");
+    }
+    cache.putIfAbsent("then", LocalTime.now());
+    var now = LocalTime.now();
+    out.printf("That was then: %s, this is now: %s.\n", cache.get("then"), now);
+    out.printf("Elapsed time: %s seconds.\n",
+               SECONDS.between(cache.get("then"), now));
     cache.close();
     return;
   }
   
   public static void main( String[] args ) {
-    new App().redisson();
+    new App().redisson(args.length > 0);
     System.exit(0);
   }
 }
